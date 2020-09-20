ba50c2ebe974ff0106b0a99a16c2ac6b3dcca2b6
Carsten Rodin <carsten.rodin@gmail.com>
2020-09-20 12:34:09 -0400

    adds feedItem page template for future json api

---
+++ src/templates/feedItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { graphql } from 'gatsby';
+// import Layout from '../layouts/default';
+
+export default function Template() {
+  return (
+    <div>FEED ITEM</div>
+  )
+};
+
+// export const pageQuery = graphql`
+//   query($slug: String!) {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     sitePage(path: { eq: $slug }) {
+//       path
+//       context {
+//         html
+//         frontmatter {
+//           slug
+//           title
+//         }
+//       }
+//     }
+//   }
+// `;
